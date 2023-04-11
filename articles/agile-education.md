655be10ec1232dc95ebe9fbf1b16452e13a0e0eb
Julian Lechner <jll38@njit.edu>
2023-04-11 16:59:09 -0400

    articles based on category

---
+++ articles/agile-education.md
@@ -0,0 +1,19 @@
+---
+title: 'Agile Education: A New Approach to Teaching and Learning'
+date: '2023-03-25'
+category: "agile-lean"
+---
+
+Agile education is a new approach to teaching and learning that is gaining popularity in today's fast-paced world. It is based on the Agile Manifesto, a set of values and principles that were originally developed for software development but have since been adapted for a variety of industries, including education.
+
+The basic premise of agile education is that learning is a continuous process that should be flexible and adaptable to the needs of individual learners. This approach emphasizes collaboration, communication, and iteration, with students and teachers working together to set goals, monitor progress, and make adjustments as needed.
+
+One of the key principles of agile education is the idea of "sprints." In a sprint, students and teachers work together to set a specific goal or objective that can be accomplished in a set amount of time, typically one to two weeks. During this time, students work collaboratively in small groups, with each group responsible for a specific aspect of the project. Teachers serve as facilitators, providing guidance and support as needed.
+
+Another key principle of agile education is the use of feedback loops. At the end of each sprint, students and teachers review their progress and identify areas for improvement. This feedback is used to inform the next sprint, with students and teachers making adjustments to their goals and strategies as needed.
+
+Agile education also emphasizes the importance of communication and collaboration. Students are encouraged to work together in small groups, with each member of the group responsible for a specific aspect of the project. Teachers serve as facilitators, providing guidance and support as needed. This approach fosters a sense of community and encourages students to develop important social and communication skills.
+
+Finally, agile education emphasizes the importance of iteration. In an agile classroom, students are encouraged to experiment and try new things, even if they don't always succeed. Teachers emphasize the process of learning over the final product, encouraging students to learn from their mistakes and make improvements over time.
+
+Overall, agile education is a promising new approach to teaching and learning that emphasizes collaboration, communication, and iteration. It encourages students to take ownership of their learning and develop important social and communication skills. As the world becomes increasingly complex and fast-paced, agile education may be just the approach we need to prepare students for the challenges of the 21st century.
--- pages/resources/academic-articles.js
+++ pages/resources/academic-articles.js
@@ -13,18 +13,43 @@ import {
   Text,
   Button,
   Image,
-  Divider
+  Divider,
+  HStack
 } from '@chakra-ui/react';
 
 export default function Articles({ allPostsData }) {
+    const agileArticles = [];
+    allPostsData.map((article, index) => {
+        if(article.category === "agile-lean")
+        {
+            agileArticles.push(article);
+        }
+    })
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Container minH={'52vh'}>
+      <Container minH={'52vh'} mx='10' w='1000px'>
+        <Heading fontSize={{ base: '2xl', sm: '3xl', lg: '5xl' }} mb={'5'}>Academic Articles</Heading>
+      <section className={` ${utilStyles.articleSections}`}>
+        <Heading fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>Agile and Lean Principles in Education</Heading>
+        <Divider mb='5'></Divider>
+        <ul className={utilStyles.list}>
+          {agileArticles.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`articles/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <Heading className={utilStyles.headingLg}>Articles</Heading>
+        <Heading fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>Neuroscience and Education</Heading>
         <Divider mb='5'></Divider>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -38,6 +63,55 @@ export default function Articles({ allPostsData }) {
           ))}
         </ul>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <Heading fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>AI and the Fourth Industrial Revolution</Heading>
+        <Divider mb='5'></Divider>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <Heading fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>Innovative Teaching Methods</Heading>
+        <Divider mb='5'></Divider>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <Heading fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>Cognitive and Mental Health Challenges</Heading>
+        <Divider mb='5'></Divider>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      
     </Container>
       
     </Layout>
