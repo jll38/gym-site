e5b7258dac92b314fbb31734874c006c7d33fd42
Julian Lechner <jll38@njit.edu>
2023-04-19 16:38:21 -0400

    GA4

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,19 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { initGA, logPageView } from "../lib/analytics";
 import { ChakraProvider } from "@chakra-ui/react";
-import useGoogleAnalytics from "../lib/useGoogleAnalytics";
-
 function MyApp({ Component, pageProps }) {
 
-  useGoogleAnalytics('G-D8VNDF0M79');
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ChakraProvider>
