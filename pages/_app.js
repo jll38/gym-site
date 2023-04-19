30c4afa16fe5d2864e6ec267c1d95a67b4bdbd3b
Julian Lechner <jll38@njit.edu>
2023-04-19 16:16:12 -0400

    Modifying GA code

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,26 @@
-import '../styles/global.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { initGA, logPageView } from '../lib/analytics'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { initGA, logPageView } from "../lib/analytics";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }) {
-  const router = useRouter()
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
   useEffect(() => {
-    initGA()
-    logPageView()
-    router.events.on('routeChangeComplete', logPageView)
+    initGA();
+    logPageView();
+    router.events.on("routeChangeComplete", logPageView);
 
     return () => {
-      router.events.off('routeChangeComplete', logPageView)
-    }
-  }, [])
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, []);
 
   return (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-  )
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
