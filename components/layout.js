35e52cc6d75518bc69d685231764137911583b52
davidfelix <ddf26@njit.edu>
2023-04-20 17:24:52 -0400

    Added OpenGraph and Twitter Meta Tags

--- components/layout.js
+++ components/layout.js
@@ -17,18 +17,11 @@ export default function Layout({ children, home }) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Pioneering the Future of Education using AI"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content="Empowering the Future of Education" />
+        <meta property="og:image" content="https://mywebclassorg.herokuapp.com/images/mywebclass_logo-1.png" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="https://mywebclassorg.herokuapp.com/images/mywebclass_logo-1.png" />
       </Head>
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
