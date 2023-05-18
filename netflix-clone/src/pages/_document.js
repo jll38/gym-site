f5c6e4f556ad2e50637879280279536372fcc36a
Julian Lechner <jll38@njit.edu>
2023-05-18 11:49:48 -0400

    Initial commit | Navbar

---
+++ netflix-clone/src/pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">  
+      <Head>
+        <script
+          defer
+          src="https://kit.fontawesome.com/a7908c27f8.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
