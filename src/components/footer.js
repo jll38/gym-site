50410454424f98b665e39f28fcbcac057dfde03c
Julian Lechner <jll38@njit.edu>
2023-06-02 11:59:57 -0400

    fixed missing footer component

---
+++ src/components/footer.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Link from "next/link";
+export function Footer({}) {
+  return <footer className="bg-black/95">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+          <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+              <Link href="/" className="flex items-center">
+                <img src="/images/atlas-logo.png" className="h-32 mr-3" alt="Atlas Gym Logo" />
+              </Link>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                <h2 className="mb-6 text-sm font-semibol uppercase text-white">
+                  Services
+                </h2>
+                <ul className="text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline">
+                      Request Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">
+                      Memberships
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold uppercase text-white">
+                  Follow us
+                </h2>
+                <ul className="text-gray-400  font-medium">
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline ">
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">
+                      Facebook
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold  uppercase text-white">
+                  Legal
+                </h2>
+                <ul className="text-gray-400  font-medium">
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline">
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">
+                      Terms &amp; Conditions
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="my-6 h-[2px] bg-gradient-to-r from-red-500 to-orange-600 sm:mx-auto lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-200 sm:text-center ">
+              © 2023{" "}
+              <Link href="/" className="hover:underline">
+                Atlas Gym™
+              </Link>
+              . All Rights Reserved.
+            </span>
+            <div className="text-sm flex mt-4sm:justify-center sm:mt-0 text-gray-200 flex-col">
+              <div className="text-lg font-medium">Contact</div>
+              <a href="mailto:example@example.com">management@atlasgym.com</a>
+            </div>
+          </div>
+        </div>
+      </footer>;
+}
+  
