e23241d33f890dba3b60525eabc230b38254e8cd
Julian Lechner <jll38@njit.edu>
2023-06-03 18:21:12 -0400

    navbar transition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,37 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useEffect } from "react";
 export function Navbar({}) {
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementById('nav');
+      if (window.scrollY > 10) {
+        navbar.classList.add('bg-black');
+      } else {
+        navbar.classList.remove('bg-black');
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
   return (
     <nav
       id="nav"
-      className="hidden sm:flex justify-start bg-black px-10 text-gray-300 gap-5 shadow-md shadow-gray-900/75"
+      className="hidden fixed sm:flex justify-start px-10 text-gray-300 gap-5 z-50 w-screen transition-all duration-300"
     >
-      <div className="ml-6 sm:ml-[16%] h-20 w-36 relative">
+      <div className="ml-6 sm:ml-[16%] h-16 w-36 relative">
         <Link href="/">
           <Image
             className="object-contain"
             src="/images/atlas-logo.png"
-            layout="fill"
+            fill
           />
         </Link>
       </div>
